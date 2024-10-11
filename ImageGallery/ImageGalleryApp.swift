ef47af85e9fa64a2910517e8aa5987125e47d365
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-11 23:13:30 +0200

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Jean on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
